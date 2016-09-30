0d46eb84eabe4d02ab33cf00074d196731917796
Gopinath krishnamoorthy <gopinathrk@in.ibm.com>
2016-09-30 07:14:14 -0500

    updated with new apis

--- app.js
+++ app.js
@@ -18,19 +18,26 @@ app.use(morgan('dev'));
 app.post('/api/auth/token', function (req, res) {
 	var header=req.headers;
 	console.log(JSON.stringify(req.body));
-	res.json({isauthenticated:true,accessToken :"se43" ,  refreshToken : "dsdew" ,  issuedAt : "25aug",expiresAfter : "1474130125000" ,  userName : "jismisimon",  userRole : "developer",properties : {defaultTimeout :"900"},clientSupport : { state : "supported/deprecated/withdrawn" , message :"Success" , upgradeUrl :"" },serverSupport : {   state :"active" ,sucmessage :"Serverup",errmessage:"Server Down" }});
+	res.json({isauthenticated:true,accessToken :"se43" ,  refreshToken : "dsdew" ,  issuedAt : "25aug",expiresAfter : 1475238618000,  userName : "jismisimon",  userRole : "developer",properties : {defaultTimeout :"900"},clientSupport : { state : "supported/deprecated/withdrawn" , message :"Success" , upgradeUrl :"" },serverSupport : {   state :"active" ,sucmessage :"Serverup",errmessage:"Server Down" }});
 });
 app.put('/api/auth/token', function (req, res) {
-	res.json({accessToken :"se43" ,  refreshToken : "dsdew" ,  issuedAt : "25aug",expiresAfter : "1474436698000" });
+	res.json({accessToken :"se43" ,  refreshToken : "dsdew" ,  issuedAt : "25aug",expiresAfter : 1475238618000});
 });
 //3. API to get Sizing Categories
 app.get('/api/categories',function(req,res){
 	res.send('{ "brands" : [{ "brandId" : "Power8" , "brand" : "Power8" , "operatingSystems" : [{ "osId" : "linux" , "os" : "Linux" , "solutions" : [{ "solutionId" : "HANA DB" , "solution" : "HANA DB"} , { "solutionId" : "HANA DB & App Server" , "solution" : "HANA DB & App Server"} ]} , {"osId" : "linux2" , "os" : "Linux2" , "solutions" : [{ "solutionId" : "HANA DB2" , "solution" : "HANA DB2"} , { "solutionId" : "HANA DB & App Server2" , "solution" : "HANA DB & App Server2"} ]} ]} , {"brandId" : "testBrand" , "brand" : "testBrand" , "operatingSystems" : [{ "osId" : "testBrandlinux" , "os" : "testBrandLinux" , "solutions" : [{ "solutionId" : "testBrandHANA DB" , "solution" : "testBrandHANA DB"} , { "solutionId" : "testBrandHANA DB & App Server" , "solution" : "testBrandHANA DB & App Server"} ]} , {"osId" : "testBrandlinux2" , "os" : "testBrandLinux2" , "solutions" : [{ "solutionId" : "testBrandHANA DB2" , "solution" : "testBrandHANA DB2"} , { "solutionId" : "testBrandHANA DB & App Server2" , "solution" : "testBrandHANA DB & App Server2"} ]}]}]}');
-}); 
+});
 app.get('/login',function(req,res){
 	res.send("inside login");
 });
+app.delete('/api/teams/:teamId/sizings/:sizingId',function(req,res){
+	res.send('{"teamId" :1,"sizingId":"1244"}');
+});
+app.post('/api/teams/:teamId/sizings',function(req,res){
+	res.send('{"teamId" :3,"sizingId":"1244"}');
+});
 app.get('/api/teams',function(req,res){
+	console.log(req.body);
 	res.send({teams : [{
 			teamId :  1 ,
 			team :  "Team A" ,
@@ -102,21 +109,313 @@ app.delete('/api/teams/:teamId',function(req,res){
 		}
 	]});
 });
+//4. API to get Sizing Properties
+app.get('/api/hana/properties',function(req,res){
+	res.send('{ "hanaProps" : { "minMemory" : "5" , "maxMemory" : "20" , "minSaps" :  "01" , "maxSaps" :  "1000" , "storageFactor" : "2" , "hanaReleases" : ["SPS11"], "sapReleases" : ["6.7"]} , "industries" : [ "Communications" ], "countries" : [{ "countryCode" : "IN" , "country" : "INDIA" }]}');
+});
+//5.API to search HANA DB Sizings
+app.get('/api/hana/db/sizings',function(req,res){
+	res.send({
+	"totalCount" :  4,
+	"items" : [{
+			"sizingHeader" : {
+				"sizingIdDisplay" :  "SB-32131-01",
+				"sizingId" :  "32131" ,
+				"sizingVersion" :  "01" ,
+				"sizingTimestamp" :  "2011-06-29T16:52:48.000Z",
+				"sizingType" :  "new" ,
+				"countryCode" :  "IN" ,
+				"industry" :  "Communications" ,
+				"brandId" :  "1" ,
+				"osId" :  "1" ,
+				"solutionId" :  "Hana DB" ,
+				"growthFactor" : {
+					"pct" :  "1" ,
+					"years" :  "2016"
+				},
+				"disk" : {
+					"diskSpace" :  "100" ,
+					"backupGenerations" :  "2"
+				},
+				"ha" : {
+					"optimization" :  "Cost Optimized" ,
+					"diskPct" :  "2"
+				},
+				"dr" : {
+					"optimization" :  "Cost Optimized" ,
+					"diskPct" :  "1"
+				}
+			},
+			"sizingRequest" : {
+				"hanaRelease" :  "1" ,
+				"hanaMemory" :  "100"
+			},
+			"nonProd" : {
+				"envs" : [{
+						"envId" :  "DEV" , //DEV, TEST, SANDBOX, PREPROD
+						"hanaMemoryPct" :  "200" ,
+						"hosting" :  "HA" , //HA, DR, STAND-ALONE
+						"stressEnvFlag" :  "Y" //Y/N
+					}
+				],
+				"diskPct" :  "3"
+			}
+		}
+	]
+});
+});
 app.delete('/api/teams/:teamId/members/:memberId',function(req,res){
 	res.send({teamId :  1,
 	team :  "TEAM A" ,
 	members:["supsanga@in.ibm.com","jissimon@in.ibm.com","anamboot@in.ibm.com"]
 });
 });
-//4. API to get Sizing Properties
-app.get('/api/hana/properties',function(req,res){
-	res.send('{ "hanaProps" : { "minMemory" : "5" , "maxMemory" : "20" , "minSaps" :  "01" , "maxSaps" :  "1000" , "storageFactor" : "2" , "hanaReleases" : ["SPS11"], "sapReleases" : ["6.7"]} , "industries" : [ "Communications" ], "countries" : [{ "countryCode" : "IN" , "country" : "INDIA" }]}');
-}); 
 app.post('/api/teams/:teamId/members',function(req,res){
 	res.send({teamId :  1,
 	team :  "TEAM A" ,
-	members:["gopinathrk@in.ibm.com","supsanga@in.ibm.com","jissimon@in.ibm.com","anamboot@in.ibm.com"]
+	members:["gopinathrk@in.ibm.com","jissimon@in.ibm.com","supsanga@in.ibm.com","anamboot@in.ibm.com"]
 });
+});
+app.post('/api/links',function(req,res){
+	res.send({"sizingIdDisplay" :  "SB-1234-01" ,
+	"sizingId" :  "1234" ,
+	"sizingVersion" :  1 ,
+	"document" : {
+		"status" :  "GENERATED" , //REQUESTED, GENERATED, FAILED
+		"linkId" :  1 , //Available if an active link is found
+		"expiresAt" :  "23 Aug 2016"//Available if link is active
+	}
+
+
+});
+});
+app.put('/api/links/:linkId',function(req,res){
+	res.send({"sizingIdDisplay" :  "SB-1234-01" ,
+	"sizingId" :  "1244" ,
+	"sizingVersion" :  1 ,
+	"document" : {
+		"status" :  "GENERATED" , //REQUESTED, GENERATED, FAILED
+		"linkId" :  2 , //Available if an active link is found
+		"expiresAt" :  "23 Aug 2016"//Available if link is active
+	}
+
+
+});
+});
+app.get('/api/sizings',function(req,res){
+	res.send({
+	"totalCount" :  "10" ,
+	"items" : [{
+			"sizingIdDisplay" :  "SB-123-01" , //Id+Version
+			"sizingId" :  "1244" ,
+			"sizingVersion" :  "1" ,
+			"sizingTimestamp" :  "2011-06-29T16:52:48.000Z" ,
+		"sizedBy" :  "gopinathrk@in.ibm.com" ,
+			"sizingType" :  "NEW" ,
+			"countryCode" :  "IN" ,
+			"industry" :  "Communications" ,
+			"brandId" :  "power8" ,
+			"brand" :  "power8" ,
+			"osId" :  "1" ,
+			"os" :  "linux" ,
+			"solutionId" :  "HANA DB" ,
+			"solution" :  "HANA DB" ,
+			"document" : {
+				"status" :  "GENERATED" , //REQUESTED, GENERATED, FAILED
+				"linkId" :  1 //Available if active link exists
+			},
+			"teams" : [{
+					"teamId" :  1 ,
+					"team" :  "Team A"
+				},
+				{
+						"teamId" :  2 ,
+						"team" :  "Team B"
+					}
+			]
+		},{
+				"sizingIdDisplay" :  "SB-1234-01" , //Id+Version
+				"sizingId" :  "1234" ,
+				"sizingVersion" :  "1" ,
+				"sizingTimestamp" :  "2011-06-29T16:52:48.000Z" ,
+			"sizedBy" :  "jissimon@in.ibm.com" ,
+				"sizingType" :  "NEW" ,
+				"countryCode" :  "IN" ,
+				"industry" :  "Communications" ,
+				"brandId" :  "power8" ,
+				"brand" :  "power8" ,
+				"osId" :  "1" ,
+				"os" :  "linux" ,
+				"solutionId" :  "HANA DB & App Server" ,
+				"solution" :  "HANA DB & App Server" ,
+				"document" : {
+					"status" :  "GENERATED" , //REQUESTED, GENERATED, FAILED
+					"linkId" : ""  //Available if active link exists
+				},
+				"teams" : [{
+						"teamId" :  1 ,
+						"team" :  "Team A"
+					},
+					{
+							"teamId" :  2 ,
+							"team" :  "Team B"
+						}
+				]
+			}
+	]
+});
+});
+app.get('/api/hana/db/sizings/:sizingId/:sizingVersion',function(req,res){
+	res.send({
+	sizingHeader : {
+		"sizingIdDisplay" :  "SB-1234-01" , //Id+Version
+		"sizingId" :  "1234" ,
+		"sizingVersion" :  "1" ,
+		"sizingTimestamp" :  "2011-06-29T16:52:48.000Z" ,
+		"sizingType" :  "NEW" ,
+		"countryCode" :  "IN" ,
+		"industry" :  "Communications" ,
+		"brandId" :  "1" ,
+		"osId" :  "1" ,
+		"solutionId" :  "HANADB"  ,
+		"growthFactor" : {
+			"pct" :  "1" ,
+			"years" :"3"
+		},
+		"disk" : {
+			"diskSpace" :  200 ,
+			"backupGenerations" :  3
+		},
+		"ha" : {
+			"optimization" :  "cost optimizied" ,
+			"diskPct" :  2
+		},
+		"dr" : {
+			"optimization" :  "cost optimizied" ,
+			"diskPct" :  1
+		}
+	},
+	"sizingRequest" : {
+		"hanaRelease" :  "1" ,
+		"hanaMemory" :  1000
+	},
+	"nonProd" : {
+		"envs" : [{
+				"envId" :  "TEST" , //DEV, TEST, SANDBOX, PREPROD
+				"hanaMemoryPct" :  200 ,
+				"hosting" :  "HA" ,
+				"stressEnvFlag" :  "Y" //Y/N
+			}
+		],
+		"diskPct" :  300
+	},
+	"sizingResults" : [{
+			"siteId" :  "HA" , //PROD, HA, DR, STAND-ALONE
+			"modelId" :  "HA" ,
+			"cpu" :  2 ,
+			"cores" :  2 ,
+			"memory" :  300 ,
+			"storage" : {
+				"ssd" :  2 ,
+				"hdd" :  2
+			},
+			"ethPort" :  4 ,
+			"sanPort" :  4 ,
+			"scaling" :  "text" ,
+			"breakup" : [{
+					"envId" :  "HA" ,
+					"components" : [{
+							"componentId" :  "123" ,
+							"component" :  "text" ,
+							"lpar" :  "0.12" ,
+							"cores" :  "0.34" ,
+							"memory" :  "200"
+						}
+					]
+				}
+			]
+		}
+	]
+})
+});
+app.get('/api/hana/dbapps/sizings/:sizingId/:sizingVersion',function(req,res){
+	res.send({
+	sizingHeader : {
+		"sizingIdDisplay" :  "SB-1234-01" , //Id+Version
+		"sizingId" :  "1234" ,
+		"sizingVersion" :  "1" ,
+		"sizingTimestamp" :  "2014:10:17T18:30:00Z" ,
+		"sizingType" :  "NEW" ,
+		"countryCode" :  "IN" ,
+		"industry" :  "Communications" ,
+		"brandId" :  "1" ,
+		"osId" :  "1" ,
+		"solutionId" :  "HANADB"  ,
+		"growthFactor" : {
+			"pct" :  "20" ,
+			"years" :  "3"
+		},
+		"disk" : {
+			"diskSpace" :  200 ,
+			"backupGenerations" :  3
+		},
+		"ha" : {
+			"optimization" :  "Cost Optimizied" ,
+			"diskPct" :  2
+		},
+		"dr" : {
+			"optimization" :  "Performance Optimizied" ,
+			"diskPct" :  1
+		}
+	},
+	"sizingRequest" : {
+		"hanaRelease" :  "SPS11" ,
+		"hanaMemory" :  200,
+		"sapRelease" :"12",
+		"appSaps":1001,
+		"tier":"2 tier"
+	},
+	"nonProd" : {
+		"envs" : [{
+				"envId" :  "TEST" , //DEV, TEST, SANDBOX, PREPROD
+				"hanaMemoryPct" :  200 ,
+				"appSapsPc":40,
+				"hosting" :  "HA" ,
+				"stressEnvFlag" :  "Y" //Y/N
+			}
+		],
+		"diskPct" :  300
+	},
+	"sizingResults" : [{
+			"name":"Production Primary Server",
+			"siteId" :  "HA" , //PROD, HA, DR, STAND-ALONE
+			"modelId" :  "HA" ,
+			"cpu" :  2 ,
+			"cores" :  2 ,
+			"memory" :  300 ,
+			"storage" : {
+				"ssd" :  2 ,
+				"hdd" :  2
+			},
+			"ethPort" :  4 ,
+			"sanPort" :  4 ,
+			"scaling" :  "text" ,
+			"breakup" : [{
+					"name": "Prod Primary Server Configuration - Breakup",
+					"envId" :  "HA" ,
+					"components" : [{
+							"componentId" :  "123" ,
+							"component" :  "text" ,
+							"lpar" :  "0.12" ,
+							"cores" :  "0.34" ,
+							"memory" :  "200"
+						}
+					]
+				}
+			]
+		}
+	]
+})
 });
 app.listen(port);
 console.log('app is listening to http://localhost:' + port);
